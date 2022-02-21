f3e0d128ad35598b9f194250b22aea1595b71484
mateus <mp.cf.pasqua@gmail.com>
2022-02-21 15:01:28 -0300

    ADD exec1

---
+++ lab01b/exec01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    int matrix[5][5], row_index, column_index, x = 0, size = 5;
+ 
+    for (row_index = 0; row_index < size; row_index++) {
+        for (column_index = 0; column_index < size; column_index++) {
+ 
+            matrix[row_index][column_index] = ++x;
+        }
+    }
+
+    printf("A matriz\n");
+    for (row_index = 0; row_index < size; row_index++) {
+        for (column_index = 0; column_index < size; column_index++) {
+ 
+            printf("%d\t", matrix[row_index][column_index]);
+        }
+ 
+        printf("\n");
+    }
+
+    printf("\nDiagonal principal:\n");
+ 
+    for (row_index = 0; row_index < size; row_index++) {
+        for (column_index = 0; column_index < size; column_index++) {
+            if (row_index == column_index)
+                printf("%d, ", matrix[row_index][column_index]);
+        }
+    }
+ 
+    return 0;
+}
