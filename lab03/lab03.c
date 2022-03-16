a1baa7b51b8bb9759bbc4cff53781d310a0c5105
mateus <mp.cf.pasqua@gmail.com>
2022-03-16 20:36:33 -0300

    Lab03Struct

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-int main() {
-    setlocale(LC_ALL, "Portuguese");
-    FILE *ptr;
-    char nomeArquivo[20], caracter;
-    scanf("%s", nomeArquivo);
-    ptr = fopen(nomeArquivo, "rt");
-    if (ptr==NULL) {
-        printf("Arquivo inexistente.");
-        exit(1);
-    }
-    caracter=getc(ptr);
-    while (caracter!=EOF){
-        printf("%c", caracter);
-        caracter=getc(ptr);
-    }
+
+typedef struct PESSOA {
+    char nome[50];
+    int idade;
+    float altura;
+}PESSOA;
+
+int comparacao(const void *pessoa1, const void *pessoa2)
+{
+    float pess1 = ((PESSOA*)pessoa1)->altura;
+    float pess2 = ((PESSOA*)pessoa2)->altura;
+    if (pess1 < pess2) return -1;
+    if (pess1 > pess2) return 1;
     return 0;
 }
+
+int main()
+{
+    struct PESSOA array[10];
+    FILE* fptr;
+    fptr = fopen("./dados.txt", "r");
+    if (fptr == NULL) 
+    {
+        printf("\nErro na abertura do arquivo\n");
+        exit(1);
+    }
+    int x = 0;
+    while (!feof(fptr))
+    {
+        fscanf(fptr, "%s", array[x].nome);
+        fscanf(fptr, "%d", array[x].idade);
+        fscanf(fptr, "%f", array[x].altura);
+        x++;
+    }
+
+    qsort(array, sizeof(PESSOA), comparacao);
+    for (int y = 0, y < x - 1; y++) 
+    {
+        printf("Nome %s", array[y].nome);
+        printf("Nome %d", array[y].idade);
+        printf("Nome %.2f", array[y].altura);
+    }
+    fclose(fptr);
+    return 0;
+
+}}
