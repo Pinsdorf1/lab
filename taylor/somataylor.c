9f5195bcea63b410bbc3d6738f54c687cba5da06
mateus <mp.cf.pasqua@gmail.com>
2022-04-25 18:51:20 -0300

    ADD SomaTaylor

---
+++ taylor/somataylor.c
@@ -0,0 +1,34 @@
+/*Grupo:
+  Calebe de Sa Ferreira | TIA: 3208816
+  Mateus de Pasquali da Silva | TIA: 32086997
+  Nicolas Pinsdorf | TIA: 32036108
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+  int x,i;
+  int fact = 1,n;
+  float soma=0;
+
+  printf("Digite quantos valores serão para ln(T):");
+  //Exemplo de In(10)
+  scanf("%d",&x);
+
+  printf("\nDigite quantos valores a série tem:");
+  //Exemplo 1/1 seria 2 (ou seja, dois números a serem divididos) a ser digitado, como na série são 10 valores que seriam somados, então o código já identifica que será a seguir série: 1/1 + 1/2 + 1/3 + 1/4 ...  1/T
+  scanf("%d",&n);
+
+  for(i=1;i<n;i++)
+  {
+    fact = fact*i;
+    soma = soma + (pow(x,i)/fact) ;
+    }
+    soma= soma +1; //Since series starts with 1
+
+    printf("\nSoma da série (aproximado) de Taylor: %.2f\n\n",soma);
+
+    return 0;
+}
