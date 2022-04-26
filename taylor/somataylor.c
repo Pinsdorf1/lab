2afabb973c05b7ed14a066f13890724a447e9089
Mateus De Pasquali da Silva <88781784+MateusPasquali@users.noreply.github.com>
2022-04-26 14:21:11 -0300

    Update somataylor.c

--- taylor/somataylor.c
+++ taylor/somataylor.c
@@ -4,31 +4,20 @@
   Nicolas Pinsdorf | TIA: 32036108
 */
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 
-int main()
-{
-  int x,y;
-  int fact = 1,n;
-  float soma=0;
+int main(void)
+{ 
+  float sum = 0;
+  float i;
 
-  printf("Digite quantos valores serão para ln(T):");
-  //Exemplo de In(10)
-  scanf("%d",&x);
-
-  printf("\nDigite quantos valores a série tem:");
-  //Exemplo 1/1 seria 2 (ou seja, dois números a serem divididos) a ser digitado, como na série são 10 valores que seriam somados, então o código já identifica que será a seguir série: 1/1 + 1/2 + 1/3 + 1/4 ...  1/T
-  scanf("%d",&n);
-
-  for(y=1;y<n;y++)
-  {
-    fact = fact*y;
-    soma = soma + (pow(x,y)/fact) ;
+  for (i = 1; i<=10; i++)
+    {
+    sum+=1/i;
     }
-    soma= soma +1; 
+  printf("Código que envolve a soma infinitesimal para solução de ln(10) em séries de Taylor ");
+  printf("\n");
+  printf("O resultado é: %f", sum);
 
-    printf("\nSoma da série (aproximado) de Taylor: %.2f\n\n",soma);
-
-    return 0;
+return 0;
 }
