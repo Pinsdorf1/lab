4368dceaa82b0577c9d8621ca9eb7cb7f3ba3212
Mateus De Pasquali da Silva <88781784+MateusPasquali@users.noreply.github.com>
2022-05-09 14:28:02 -0300

    Update v1projtaylor.py

--- V1Taylor/v1projtaylor.py
+++ V1Taylor/v1projtaylor.py
@@ -2,26 +2,40 @@
 #Calebe de Sa Ferreira | TIA: 32088116  
 #Mateus de Pasquali da Silva | TIA: 32086997
 #Nicolas Pinsdorf | TIA: 32036108
-
 from threading import Thread
 import concurrent.futures
 import time
 
-def Taylor(ln): #Função que calcula a série de Taylor 
-    taylor_Func = 0
-    for i in range(ln):
+def Taylor1(ln1):
+    S = 0
+    for i in range(ln1):
         if i == 0:
             pass
         else:
-             taylor_Func += 1/i
-    return taylor_Func
+            S += 1/i
+    return S
+def Taylor2(ln2):
+    X = 0
+    b = ln2
+    for i in range(ln2):
+        X += 1 / b
+        b += 1
+    return X
+def Taylor3(ln3):
+    Z = 0
+    c = ln3
+    for d in range(ln3):
+        Z += 1 / c
+        c += 1
+    return Z
 begin = time.time()
-
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    print('Compilando o programa!')
-    future = executor.submit(Taylor, 100)#ln = 100
-    return_value = future.result() 
-    Resultado = return_value 
-    print('Resultado = ', Resultado)
+    print('Compilando código!')
+    future = executor.submit(Taylor1, 50)
+    return_value = future.result()
+    Resultado2 = Taylor2(25)
+    Resultado3 = Taylor3(25)
+    Final_result = return_value + Resultado2
+    print('Resultado =', Final_result)
     end = time.time()
     print('Speedup: ', end - begin, 'segundos')
