ee8c555f22d05b4ed0f69408015186bd9f1bedcb
Mateus De Pasquali da Silva <88781784+MateusPasquali@users.noreply.github.com>
2022-05-16 16:06:56 -0300

    Update somataylor.c

--- V2Taylor/somataylor.c
+++ V2Taylor/somataylor.c
@@ -7,17 +7,16 @@
 #include <stdio.h>
 
 int main(void)
-{ 
-  float sum = 0;
-  float i;
+{
+  double i;
+  double sum = 0;
 
-  for (i = 1; i<=10; i++)
+  for (i = 1; i<=10000000000; i++)
     {
-    sum+=1/i;
+    sum+=1/(double)i;
     }
-  printf("Código que envolve a soma infinitesimal para solução de ln(10) em séries de Taylor ");
-  printf("\n");
-  printf("O resultado é: %f", sum);
+  printf("Código que envolve a soma infinitesimal para solução de ln(10) em séries de Taylor\n");
+  printf("O resultado é: %.3f", sum);
 
 return 0;
 }
