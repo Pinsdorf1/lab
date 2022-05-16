6b7f8d6767f08b2019af940e9ce9328d6bdf7982
Mateus De Pasquali da Silva <88781784+MateusPasquali@users.noreply.github.com>
2022-05-16 19:24:34 -0300

    Update projv2taylorwithoutcritical.c

--- V2Taylor/projv2taylorwithoutcritical.c
+++ V2Taylor/projv2taylorwithoutcritical.c
@@ -6,18 +6,19 @@ Nicolas Pinsdorf | TIA: 32036108
 */
 //Projeto V2 Série de Taylor, utilizando o OpenMP, porém sem # pragma omp critical
 //Comando para rodar o código: gcc -g -Wall -fopenmp -o nome_do_projeto nome_do_projeto.c
+// ./nome_do_projeto 6(Número de Threads)
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 
 
-long int T = 1000000000;
+long int ln_T = 1000000000;
 
 double taylor(double* resultado_final);
 
 int main(int argc, char* argv[]) {
 
-  double resultado_final = 0.0;
+  double resultado_final = 0;
   int thread_count;
 
   thread_count = strtol(argv[1], NULL, 10);
@@ -25,11 +26,11 @@ int main(int argc, char* argv[]) {
 # pragma omp parallel for num_threads(thread_count) \
     reduction(+: resultado_final)
 
-  for (long int i = 1; i <= T; i++) {
+  for (long int i = 1; i <= ln_T; i++) {
     resultado_final += 1 / (double) i;
   }
  
-  printf("ln(%ld) = %.3f\n", T, resultado_final);
+  printf("ln(T)(%ld) = %f\n", ln_T, resultado_final);
   
   return 0;
 }
